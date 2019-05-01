92b8ad3bb419f0f067ad42ee78b31f1b841244d1
WOLFTECH\rmmetoye <rmmetoye@ncsu.edu>
2019-05-01 15:35:21 -0400

    Renamed driver mfile and added a test folder.

---
+++ test/performanceTests.m
@@ -0,0 +1,10 @@
+% performanceTests.m
+% Tests the performance of the models
+
+clearvars; close all; clc;
+
+%% Each of these should have a high-perturbation, highly variable tension test and a small perturbation test
+% uniformSteadyFlow
+% uniformVariableFlow
+% gradientSteadyFlow
+% gradientUnsteadyFlow
---
+++ test/unitTests.m
@@ -0,0 +1,12 @@
+% unitTests.m
+% Tests individual modules
+
+% For now let's have a list of functions that we can comment or uncomment
+% to run the tests.
+
+% fluidUnitTests
+% nodeUnitTests
+% linkUnitTests
+% tetherUnitTests
+
+
