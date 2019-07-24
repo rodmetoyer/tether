a4cf23933677b6fcda38f8ee4825a3e02c2fe1fe
RODNEY-PC\Rodney <rmmetoye@ncsu.edu>
2019-07-24 11:12:13 -0400

    Fixed sign error and added visualization in the main
    Model seems to be working properly without any hydrodynamic loads. Next step is to add drag and structural damping.

--- sandbox/bar_main.m
+++ sandbox/bar_main.m
@@ -2,6 +2,13 @@
 
 clearvars; close all; clc;
 
+% sim setup
+sim.Duration = 60;
+sim.TimeStep = 0.01;
+
+% Results visualization setup
+makemovie = true; moviefile = 'FiveLinks_noDrag.avi'; frmrt = 10;
+
 % Build a thr struct to use in place of the class for now
 % For the class I'll make two constructors: one build up from links and one
 % build down from tether. todo(rodney)
@@ -9,7 +16,7 @@ thr.numlinks = 5;
 thr.length = 12; % m
 thr.meanDiameter = 0.1; % m
 thr.mass = 3; % kg
-thr.density = 100;
+thr.density = 900;
 env.gravity = 9.8;
 env.fluid.density = 1000;
 for i=1:1:thr.numlinks
@@ -17,7 +24,7 @@ for i=1:1:thr.numlinks
     thr.link(i).diameter = thr.meanDiameter; 
     thr.link(i).mass = thr.mass/thr.numlinks;   % kg
     thr.link(i).reldens = thr.density/env.fluid.density;
-    thr.link(i).weight = pi/4*thr.link(i).diameter^2*thr.link(i).length*env.gravity*(thr.link(i).reldens-1);
+    thr.link(i).weight = -pi/4*thr.link(i).diameter^2*thr.link(i).length*env.gravity*(thr.link(i).reldens-1); % if more dense weight is down
     temp = thr.link(i).mass*(thr.link(i).length^2 + 3/4*thr.link(i).diameter^2)/12;
     thr.link(i).inertia = [1/8*thr.link(i).mass*thr.link(i).diameter^2 0 0; 0 temp 0; 0 0 temp];
     clear temp
@@ -25,7 +32,7 @@ for i=1:1:thr.numlinks
     thr.link(i).c = thr.link(i).length - thr.link(i).x;
     thr.link(i).moment = [0;0;0];
     % Initial states
-    qj = [1;4;3;5];
+    qj = [1;0;0;0];
     qj = qj/norm(qj);
     x0(3*i-2,1) = 0;
     x0(3*i-1,1) = 0;
@@ -40,14 +47,15 @@ thr.baseMoment = [0;0;0];
 
 t = 0;
 xdot = bar_state(t,x0,thr);
-simtime = 0:0.001:30;
+simtime = 0:sim.TimeStep:sim.Duration; 
 opts = odeset('RelTol',1e-5,'Stats','on','OutputFcn',@odeplot);
 [time,y] = ode45(@(t,x) bar_state(t,x,thr),simtime,x0,opts);
 
 %% Post process
 % set-up
 N = thr.numlinks;
-for j=1:1:length(y)
+numSteps = length(y);
+for j=1:1:numSteps
     for i = 1:1:N
         qj = [y(j,3*N+4*i-3);y(j,3*N+4*i-2);y(j,3*N+4*i-1);y(j,3*N+4*i)];
         j_C_O = [qj(1)^2+qj(2)^2-qj(3)^2-qj(4)^2, 2*(qj(2)*qj(3) + qj(1)*qj(4)),   2*(qj(2)*qj(4) - qj(1)*qj(3));...
@@ -65,13 +73,77 @@ for j=1:1:length(y)
         linkz(i,j) = r_co_O(3*i,j);
     end % end post set-up loop
 end % end time loop
+% Add the ground node for plotting
+linkx = [zeros(1,length(linkx));linkx];
+linky = [zeros(1,length(linky));linky];
+linkz = [zeros(1,length(linkz));linkz];
 
+cmap = jet(N);
+% Look at the evolution of the states
+for i=1:1:N
+    figure('Position',[100 100 800 600]);
+    plot(time,y(:,3*i-2),'Marker','+','Color','r');%cmap(i,:));
+    hold on
+    plot(time,y(:,3*i-1),'Marker','o','Color','b');%cmap(i,:));
+    plot(time,y(:,3*i),'Marker','*','Color','g');%cmap(i,:));
+    hold off
+    title(['Link ' num2str(i)]);
+    xlabel('time (s)'); ylabel('angular rate (rad/s)');
+end
+
+for i=1:1:N
+    figure('Position',[600 400 800 600]);
+    plot(time,y(:,3*N+4*i-3),'Marker','+','Color','r');%cmap(i,:));
+    hold on
+    plot(time,y(:,3*N+4*i-2),'Marker','o','Color','b');%cmap(i,:));
+    plot(time,y(:,3*N+4*i-1),'Marker','*','Color','g');%cmap(i,:));
+    plot(time,y(:,3*N+4*i),'Marker','^','Color','k');%cmap(i,:));
+    hold off
+    title(['Link ' num2str(i)]);
+    xlabel('time (s)'); ylabel('Euler Parameter');
+end
+
+cmap = jet(N+1);
 % Start position
 figure
-plot3(link
+scatter3(linkx(:,1),linky(:,1),linkz(:,1),20,cmap,'filled');
+
+% mid position
+figure
+timeloc = round(0.5*length(simtime));
+scatter3(linkx(:,timeloc),linky(:,timeloc),linkz(:,timeloc),20,cmap,'filled');
 
 % End position
-
+figure
+scatter3(linkx(:,end),linky(:,end),linkz(:,end),20,cmap,'filled');
 
 % Movie
-
+if makemovie
+hfig = figure('Color','w','Position',[100 100 1000 700]);
+% numtrail = 50;
+% trmap = gray(numtrail);
+for i=1:1:frmrt*sim.Duration+1
+    n=(i-1)*(numSteps-1)/(frmrt*sim.Duration)+1;
+    for j=1:1:N
+        plot3([linkx(j,n) linkx(j+1,n)],[linky(j,n) linky(j+1,n)],[linkz(j,n) linkz(j+1,n)],'color',cmap(j,:));
+        hold on;
+    end    
+    grid on
+    axis equal; axis([-1.1*thr.length,1.1*thr.length,-1.1*thr.length,1.1*thr.length,-1.1*thr.length,1.1*thr.length]);
+    xlabel('x(m)');ylabel('y(m)');zlabel('z(m)');
+    title(['Time: ' num2str(time(n),'%4.2f')]);
+    hold off
+    M(i) = getframe(hfig);
+end
+% todo do we want to add a dlg to choose a folder to save to?
+if ~exist([pwd '\products'],'dir')
+    mkdir('products');
+end
+if ~exist([pwd '\products\videos'],'dir')
+    mkdir('products\videos');
+end
+moviefile = ['products\videos\' moviefile];
+writerObj = VideoWriter(moviefile);
+writerObj.FrameRate = frmrt; writerObj.Quality = 100; % optional
+open(writerObj); writeVideo(writerObj,M); close(writerObj);
+end
--- sandbox/bar_state.m
+++ sandbox/bar_state.m
@@ -23,8 +23,8 @@ for i=1:1:N
     rjX = [0 0 0; 0 0 -thr.link(i).x; 0 thr.link(i).x 0;];
     T(3*i-2:3*i,3*i-2:3*i) = rjX;
     if i == 1
-        velLinkCM = omegajX*[thr.link(i).x;0;0];
-        velLinkCMprevious = velLinkCM;
+        %velLinkCM = omegajX*[thr.link(i).x;0;0];
+        %velLinkCMprevious = velLinkCM;
         j_C_k = j_C_O;
         ST(3*i-2:3*i,1) = -omegajX*(omegajX*[thr.link(i).x;0;0]); % ST is 3Nx1 vector
     else
@@ -32,18 +32,16 @@ for i=1:1:N
         omegajXminus = [0 -omegajminus(3) omegajminus(2);...
                omegajminus(3) 0 -omegajminus(1);...
                -omegajminus(2) omegajminus(1) 0];
-        velLinkEndprevious = velLinkCMprevious + omegajXminus*[thr.link(i-1).c;0;0];
-        velLinkCM = velLinkEndprevious + omegajX*[thr.link(i).x;0;0];
-        velLinkCMprevious = velLinkCM;
+        %velLinkEndprevious = velLinkCMprevious + omegajXminus*[thr.link(i-1).c;0;0];
+        %velLinkCM = velLinkEndprevious + omegajX*[thr.link(i).x;0;0];
+        %velLinkCMprevious = velLinkCM;
         qj = [x(3*N+4*(i-1)-3) x(3*N+4*(i-1)-2) x(3*N+4*(i-1)-1) x(3*N+4*(i-1))];
         k_C_O = [qj(1)^2+qj(2)^2-qj(3)^2-qj(4)^2, 2*(qj(2)*qj(3) + qj(1)*qj(4)),   2*(qj(2)*qj(4) - qj(1)*qj(3));...
                  2*(qj(2)*qj(3) - qj(1)*qj(4)),   qj(1)^2-qj(2)^2+qj(3)^2-qj(4)^2, 2*(qj(3)*qj(4) + qj(1)*qj(2));...
                  2*(qj(2)*qj(4) + qj(1)*qj(3)),   2*(qj(3)*qj(4) - qj(1)*qj(2)),   qj(1)^2-qj(2)^2-qj(3)^2+qj(4)^2];
         j_C_k = j_C_O*transpose(k_C_O);
-        if i < N
-            CA(3*i+1:3*i+3,3*i-2:3*i) = -j_C_k; % Adds j_C_k to diagonal edge of the lower triangle - everything else is 0
-            T(3*i+1:3*i+3,3*i-2:3*i) = j_C_k*rjX;        
-        end        
+        T(3*i-2:3*i,3*(i-1)-2:3*(i-1)) = j_C_k*rjXprevious;
+        CA(3*i-2:3*i,3*(i-1)-2:3*(i-1)) = -j_C_k; % Adds j_C_k to diagonal edge of the lower triangle - everything else is 0
         ST(3*i-2:3*i,1) = -j_C_k*omegajXminus*(omegajXminus*[thr.link(i-1).c;0;0]) - omegajX*(omegajX*[thr.link(i).x;0;0]);
     end    
     I(3*i-2:3*i,3*i-2:3*i) = thr.link(i).inertia; % puts the inertia matrix on the diagonal - everything else is 0    
@@ -58,20 +56,22 @@ for i=1:1:N
     end    
     bigOmegaX(4*i-3:4*i,4*i-3:4*i) = [0 -omegaj(1) -omegaj(2) -omegaj(3);...
                                       omegaj(1) 0 omegaj(3) -omegaj(2);...
-                                      -omegaj(2) -omegaj(3) 0 omegaj(1);...
+                                      omegaj(2) -omegaj(3) 0 omegaj(1);...
                                       omegaj(3) omegaj(2) -omegaj(1) 0];
     % Build last of the matrices. The ones that only have stuff at the end
     SF(3*i-2:3*i,3*i-2:3*i) = zeros;
     FL(3*i-2:3*i,1) = [0;0;0];
     L(3*i+1:3*i+3,1) = thr.link(i).moment;
     Somega(3*i-2:3*i,1) = omegajX*(thr.link(i).inertia*omegaj);
-    FD = computeExternalForces(transpose(j_C_O)*velLinkCM,j_C_O);
+    %FD = computeExternalForces(transpose(j_C_O)*velLinkCM,j_C_O); % todo fix drag function 
+    FD = [0;0;0];
     FT(3*i-2:3*i,1) = FD + [0;0;thr.link(i).weight];
     if i==N
-        SF(3*i-2:3*i,3*i-2:3*i) = rjX*j_C_O;
+        SF(3*i-2:3*i,3*i-2:3*i) = -rjX*j_C_O;
         FL(3*i-2:3*i,1) = thr.endForce;
         FT(3*i-2:3*i,1) = FT(3*i-2:3*i,1) + thr.endForce;
-    end    
+    end
+    rjXprevious = rjX;
 end % end links loop
 % disp('test complete');
 xdot = NaN(7*N,1);
@@ -84,6 +84,7 @@ temp3 = M*CAinvST+CF*FT;
 CRinvtemp3 = CR\temp3;
 temp4 = temp2-SR*CRinvtemp3;
 xdot(1:3*N) = temp1\temp4;
+%xdot(1:3*N) = inv(I+SR*inv(CR)*M*inv(CA)*T)*(SF*FL+CL*L-Somega-SR*inv(CR)*(M*inv(CA)*ST+CF*FT));
 xdot(3*N+1:7*N) = 0.5*bigOmegaX*x(3*N+1:7*N);
 end % end bar_state
 
@@ -94,6 +95,6 @@ function f = computeExternalForces(velocity,orientation)
     % todo(rodney) add environment argument to get flow speed
     tempFlowSpeed = [0;0;0];
     relVel = velocity - tempFlowSpeed;
-    shittyViscosityCoefficent = 0.05;
+    shittyViscosityCoefficent = 0.0;
     f = -shittyViscosityCoefficent*relVel;
 end % end computeExternalForces
