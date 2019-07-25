d807fdde8f80274b879b071b389e4ba1a1194b1d
RODNEY-PC\Rodney <rmmetoye@ncsu.edu>
2019-07-25 11:24:36 -0400

    Fixed a bug in the tether set-up. Corrected an equation. Added more visualization. Still getting odd behavior.

--- sandbox/bar_main.m
+++ sandbox/bar_main.m
@@ -4,21 +4,22 @@ clearvars; close all; clc;
 
 % sim setup
 sim.Duration = 60;
-sim.TimeStep = 0.005;
+sim.TimeStep = 0.001;
 
 % Results visualization setup
-makemovie = true; moviefile = 'SevenLinks100m_noDrag.avi'; frmrt = 10;
+makemovie = true; moviefile = 'test_noDrag.avi'; frmrt = 20;
+saveplots = true;
 
 % Build a thr struct to use in place of the class for now
 % For the class I'll make two constructors: one build up from links and one
 % build down from tether. todo(rodney)
-thr.numlinks = 7;
+thr.numlinks = 10;
 thr.length = 100; % m
 thr.meanDiameter = 0.024; % m
 thr.density = 769.25; % kg/m^3 based on 24mm Spectra 12 braid
 thr.mass = pi/4*thr.meanDiameter^2*thr.length*thr.density; % kg
 env.gravity = 9.8;
-env.fluid.density = 1.225; % air 1.225, water 997
+env.fluid.density = 400; % air 1.225, water 997
 for i=1:1:thr.numlinks
     thr.link(i).length = thr.length/thr.numlinks; % m
     thr.link(i).diameter = thr.meanDiameter; 
@@ -28,7 +29,7 @@ for i=1:1:thr.numlinks
     temp = thr.link(i).mass*(thr.link(i).length^2 + 3/4*thr.link(i).diameter^2)/12;
     thr.link(i).inertia = [1/8*thr.link(i).mass*thr.link(i).diameter^2 0 0; 0 temp 0; 0 0 temp];
     clear temp
-    thr.link(i).x = 0.1;
+    thr.link(i).x = 0.5*thr.link(i).length;
     thr.link(i).c = thr.link(i).length - thr.link(i).x;
     thr.link(i).moment = [0;0;0];
     % Initial states
@@ -80,42 +81,95 @@ linkz = [zeros(1,length(linkz));linkz];
 
 cmap = jet(N);
 % Look at the evolution of the states
+if saveplots; color = 'none'; else; color = 'w'; end
 for i=1:1:N
-    figure('Position',[100 100 800 600]);
+    figure('Position',[100 100 800 600],'color',color);
     plot(time,y(:,3*i-2),'Marker','+','Color','r');%cmap(i,:));
     hold on
     plot(time,y(:,3*i-1),'Marker','o','Color','b');%cmap(i,:));
     plot(time,y(:,3*i),'Marker','*','Color','g');%cmap(i,:));
     hold off
-    title(['Link ' num2str(i)]);
+    title(['Angular Rates | Link ' num2str(i)]);
     xlabel('time (s)'); ylabel('angular rate (rad/s)');
+    if saveplots
+        set(gca,'color','none');
+        export_fig(['products\rates_Link' num2str(i)], '-png', '-transparent');
+        close gcf
+    end
 end
 
 for i=1:1:N
-    figure('Position',[600 400 800 600]);
+    figure('Position',[600 400 800 600],'color',color);
     plot(time,y(:,3*N+4*i-3),'Marker','+','Color','r');%cmap(i,:));
     hold on
     plot(time,y(:,3*N+4*i-2),'Marker','o','Color','b');%cmap(i,:));
     plot(time,y(:,3*N+4*i-1),'Marker','*','Color','g');%cmap(i,:));
     plot(time,y(:,3*N+4*i),'Marker','^','Color','k');%cmap(i,:));
     hold off
-    title(['Link ' num2str(i)]);
+    title(['Euler Parameters | Link ' num2str(i)]);
     xlabel('time (s)'); ylabel('Euler Parameter');
+    if saveplots
+        set(gca,'color','none');
+        export_fig(['products\EulerParams_Link' num2str(i)], '-png', '-transparent');
+        close gcf
+    end
 end
 
 cmap = jet(N+1);
 % Start position
-figure
-scatter3(linkx(:,1),linky(:,1),linkz(:,1),20,cmap,'filled');
+figure('Position',[600 400 800 600],'color',color);
+plot3([linkx(1,1) linkx(1+1,1)],[linky(1,1) linky(1+1,1)],[linkz(1,1) linkz(1+1,1)],'color',cmap(1,:),'LineWidth',2.5);
+hold on
+for j=2:1:N    
+    plot3([linkx(j,1) linkx(j+1,1)],[linky(j,1) linky(j+1,1)],[linkz(j,1) linkz(j+1,1)],'color',cmap(j,:),'LineWidth',2.5);    
+end
+hold off
+axis([-1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length])
+axis equal
+title('Start Position'); xlabel('x(m)'); ylabel('y(m)'); zlabel('z(m)');
+if saveplots
+    set(gca,'color','none');
+    export_fig('products\startPosition', '-png', '-transparent');
+    close gcf
+end
+%scatter3(linkx(:,1),linky(:,1),linkz(:,1),20,cmap,'filled');
 
 % mid position
-figure
-timeloc = round(0.5*length(simtime));
-scatter3(linkx(:,timeloc),linky(:,timeloc),linkz(:,timeloc),20,cmap,'filled');
+timeloc = round(0.5*length(numSteps));
+figure('Position',[600 400 800 600],'color',color);
+plot3([linkx(1,timeloc) linkx(1+1,timeloc)],[linky(1,timeloc) linky(1+1,timeloc)],[linkz(1,timeloc) linkz(1+1,timeloc)],'color',cmap(1,:),'LineWidth',2.5);
+hold on
+for j=1:1:N
+    plot3([linkx(j,timeloc) linkx(j+1,timeloc)],[linky(j,timeloc) linky(j+1,timeloc)],[linkz(j,timeloc) linkz(j+1,timeloc)],'color',cmap(j,:),'LineWidth',2.5);
+end
+hold off
+axis([-1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length])
+axis equal
+title('Mid-Sim Position Position'); xlabel('x(m)'); ylabel('y(m)'); zlabel('z(m)');
+if saveplots
+    set(gca,'color','none');
+    export_fig('products\midSimPosition', '-png', '-transparent');
+    close gcf
+end
+% scatter3(linkx(:,timeloc),linky(:,timeloc),linkz(:,timeloc),20,cmap,'filled');
 
 % End position
-figure
-scatter3(linkx(:,end),linky(:,end),linkz(:,end),20,cmap,'filled');
+figure('Position',[600 400 800 600],'color',color);
+plot3([linkx(1,end) linkx(1+1,end)],[linky(1,end) linky(1+1,end)],[linkz(1,end) linkz(1+1,end)],'color',cmap(1,:),'LineWidth',2.5);
+hold on
+for j=1:1:N
+    plot3([linkx(j,end) linkx(j+1,end)],[linky(j,end) linky(j+1,end)],[linkz(j,end) linkz(j+1,end)],'color',cmap(j,:),'LineWidth',2.5);
+end
+%scatter3(linkx(:,end),linky(:,end),linkz(:,end),20,cmap,'filled');
+hold off
+axis([-1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length -1.1*thr.length 1.1*thr.length])
+axis equal
+title('End Position'); xlabel('x(m)'); ylabel('y(m)'); zlabel('z(m)');
+if saveplots
+    set(gca,'color','none');
+    export_fig('products\endPosition', '-png', '-transparent');
+    close gcf
+end
 
 % Movie
 if makemovie
--- sandbox/bar_state.m
+++ sandbox/bar_state.m
@@ -21,6 +21,7 @@ for i=1:1:N
     CF(3*i-2:3*i,3*i-2:3*i) = j_C_O;  % puts rotation matices on the diagonal
     CA(3*i-2:3*i,3*i-2:3*i) = eye(3); % puts identity matices on the diagonal
     rjX = [0 0 0; 0 0 -thr.link(i).x; 0 thr.link(i).x 0;];
+    cjX = [0 0 0; 0 0 -thr.link(i).c; 0 thr.link(i).c 0;];
     T(3*i-2:3*i,3*i-2:3*i) = rjX;
     if i == 1
         %velLinkCM = omegajX*[thr.link(i).x;0;0];
@@ -36,10 +37,12 @@ for i=1:1:N
         %velLinkCM = velLinkEndprevious + omegajX*[thr.link(i).x;0;0];
         %velLinkCMprevious = velLinkCM;
         qj = [x(3*N+4*(i-1)-3) x(3*N+4*(i-1)-2) x(3*N+4*(i-1)-1) x(3*N+4*(i-1))];
+        qj = qj/norm(qj);
         k_C_O = [qj(1)^2+qj(2)^2-qj(3)^2-qj(4)^2, 2*(qj(2)*qj(3) + qj(1)*qj(4)),   2*(qj(2)*qj(4) - qj(1)*qj(3));...
                  2*(qj(2)*qj(3) - qj(1)*qj(4)),   qj(1)^2-qj(2)^2+qj(3)^2-qj(4)^2, 2*(qj(3)*qj(4) + qj(1)*qj(2));...
                  2*(qj(2)*qj(4) + qj(1)*qj(3)),   2*(qj(3)*qj(4) - qj(1)*qj(2)),   qj(1)^2-qj(2)^2-qj(3)^2+qj(4)^2];
         j_C_k = j_C_O*transpose(k_C_O);
+        rjXprevious = [0 0 0; 0 0 -thr.link(i-1).x; 0 thr.link(i-1).x 0;];
         T(3*i-2:3*i,3*(i-1)-2:3*(i-1)) = j_C_k*rjXprevious;
         CA(3*i-2:3*i,3*(i-1)-2:3*(i-1)) = -j_C_k; % Adds j_C_k to diagonal edge of the lower triangle - everything else is 0
         ST(3*i-2:3*i,1) = -j_C_k*omegajXminus*(omegajXminus*[thr.link(i-1).c;0;0]) - omegajX*(omegajX*[thr.link(i).x;0;0]);
@@ -51,7 +54,7 @@ for i=1:1:N
     CL(3*i-2:3*i,3*i+1:3*i+3) = -eye(3); % Adds rjX to diagonal edge of the upper triangle to finish with 3N X 3N+3 matrix
     M(3*i-2:3*i,3*i-2:3*i) = thr.link(i).mass*eye(3);
     if i < N
-        SR(3*i-2:3*i,3*i+1:3*i+3) = rjX; % Adds rjX to diagonal edge of the upper triangle - everything else is 0
+        SR(3*i-2:3*i,3*i+1:3*i+3) = cjX; % Adds rjX to diagonal edge of the upper triangle - everything else is 0
         CR(3*i-2:3*i,3*i+1:3*i+3) = -eye(3);
     end    
     bigOmegaX(4*i-3:4*i,4*i-3:4*i) = [0 -omegaj(1) -omegaj(2) -omegaj(3);...
@@ -71,7 +74,7 @@ for i=1:1:N
         FL(3*i-2:3*i,1) = thr.endForce;
         FT(3*i-2:3*i,1) = FT(3*i-2:3*i,1) + thr.endForce;
     end
-    rjXprevious = rjX;
+    %rjXprevious = rjX;
 end % end links loop
 % disp('test complete');
 xdot = NaN(7*N,1);
